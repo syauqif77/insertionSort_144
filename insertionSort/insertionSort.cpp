46c57534c26ea3459250c340ead184041330bf71
syauqif77 <syagfm777@gmail.com>
2023-03-23 16:35:13 +0700

    tambahan fungsi void display

--- insertionSort/insertionSort.cpp
+++ insertionSort/insertionSort.cpp
@@ -73,6 +73,14 @@ void display()
     cout << "element array yang telah tersusun" << endl; // output ke layar
     cout << "===================" << endl; // output ke layar
 
+    for (int j = 0; j < n; j++)
+    {                           // looping dengan j dimulai dari 0 hingga n-1
+        cout << arr[j] << endl; // output ke layar
+
+
+    }
+    cout << endl;   // output baris kosong
+}
 
 // procedure insertionsort
 
