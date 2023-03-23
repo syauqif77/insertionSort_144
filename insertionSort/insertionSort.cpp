a96cb00e25eb949ffefa6f70d91e59ab46c1b023
syauqif77 <syagfm777@gmail.com>
2023-03-23 16:21:55 +0700

    tambah fungsi array

--- insertionSort/insertionSort.cpp
+++ insertionSort/insertionSort.cpp
@@ -5,7 +5,11 @@
 #include <iostream>
 using namespace std;
 
+int arr[20];    // membuat array dengan panjang data 20
+int n;          // membuat variabel inputan n
+
 int main()
+
 {
     std::cout << "Hello World!\n";
 }
