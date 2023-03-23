90a221ab37fb99b356c994e164672c085e0f51f0
syauqif77 <syagfm777@gmail.com>
2023-03-23 16:22:35 +0700

    tambah void input

--- insertionSort/insertionSort.cpp
+++ insertionSort/insertionSort.cpp
@@ -8,6 +8,17 @@ using namespace std;
 int arr[20];    // membuat array dengan panjang data 20
 int n;          // membuat variabel inputan n
 
+void input()
+{   // procedure input
+    while (true)
+    {
+        cout << "masukkan jumlah data pada array : "; // membuat inputan jumlah element array
+        cin >> n;                                     // memanggil variabel inputan n
+
+        if (n <= 20)
+        {   // membuat kondisi n tidak lebih dari 20
+            break;
+
 int main()
 
 {
